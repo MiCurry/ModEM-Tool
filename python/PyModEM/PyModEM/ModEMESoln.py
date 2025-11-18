01ec9faacfc2a3f1d0bb88b7492949b192bf1291
Miles Curry <2590700+MiCurry@users.noreply.github.com>
2025-11-18 15:26:34 -0700

    Implement a class to handle ESoln files
    It reads and writes ESoln files.

---
+++ python/PyModEM/PyModEM/ModEMESoln.py
@@ -0,0 +1,88 @@
+import struct
+from dataclasses import dataclass
+import numpy as np
+
+
+@dataclass
+class ESolnData:
+    gridType : str
+    nx : int
+    ny : int
+    nz : int
+    x : np.ndarray
+    y : np.ndarray 
+    z : np.ndarray
+
+class ESoln:
+
+    def __init__(self):
+        
+        self._data = ESolnData(
+            gridType=None,
+            nx=0,
+            ny=0,
+            nz=0,
+            x=None,
+            y=None,
+            z=None
+        )
+
+    @property
+    def nx(self) -> int:
+        return self._data.nx
+
+    @property
+    def ny(self) -> int:
+        return self._data.ny
+
+    @property
+    def nz(self) -> int:
+        return self._data.nz
+
+    @property
+    def gridType(self) -> str:
+        return self._data.gridType
+
+    @property
+    def x(self) -> np.ndarray:
+        return self._data.x
+
+    @property
+    def y(self) -> np.ndarray:
+        return self._data.y
+
+    @property
+    def z(self) -> np.ndarray:
+        return self._data.z
+
+    def read(self, fname):
+        with open(fname, 'rb') as file:
+            _ = struct.unpack('i', file.read(4)) # Fortran record header - skip
+            self._data.nx = struct.unpack('i', file.read(4))[0]
+            self._data.ny = struct.unpack('i', file.read(4))[0]
+            self._data.nz = struct.unpack('i', file.read(4))[0]
+            self._data.gridType = str(file.read(80).decode('utf-8'))
+
+            _ = struct.unpack('i', file.read(4)) # Fortran record handle - skip
+            xs = struct.iter_unpack('D', file.read(self.nx*16))
+            ys = struct.iter_unpack('D', file.read(self.ny*16))
+            zs = struct.iter_unpack('D', file.read(self.nz*16))
+
+            self._data.x = np.array([x[0] for x in xs])
+            self._data.y = np.array([y[0] for y in ys])
+            self._data.z = np.array([z[0] for z in zs])
+
+    def write(self, fname):
+        with open(fname, 'w') as file:
+            file.write(str(self.nx) + '\n')
+            file.write(str(self.ny) + '\n')
+            file.write(str(self.nz) + '\n')
+            file.write(self.gridType + '\n')
+
+            np.savetxt(file, self.x, delimiter=',')
+            file.write('\n')
+            np.savetxt(file, self.y, delimiter=',')
+            file.write('\n')
+            np.savetxt(file, self.z, delimiter=',')
+
+        return fname
--- python/PyModEM/setup.py
+++ python/PyModEM/setup.py
@@ -16,14 +16,11 @@ setup(
     packages=["PyModEM"],
     scripts=["scripts/make_mesh",
             "scripts/make_modem_data",
-            "scripts/modem_mesh_dump",
             "scripts/plot_mesh",
             "scripts/plot_mesh_2d",
             "scripts/plot_mesh_3d",
-            "scripts/plot_mesh_3d_plotly",
             "scripts/read_mesh",
             "scripts/read_modem_data",
-            "scripts/sort_report",
             "scripts/synth_error",
             "scripts/modem_cov"
             ]
